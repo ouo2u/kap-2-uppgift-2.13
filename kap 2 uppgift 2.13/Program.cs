395a81c564e10682de22d0e8ce930751cedd9a34
ouo2u <abod98451@gmail.com>
2022-09-14 15:03:44 +0200

    Add project files.

---
+++ kap 2 uppgift 2.13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace kap_2_uppgift_2._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vad är lönen för den första anställd? ");
+            string anställd1 = (Console.ReadLine());
+            int lön1 = int.Parse(anställd1);
+            Console.WriteLine("Vad är lönen för den andra anställd? ");
+            string anställd2 = (Console.ReadLine());
+            int lön2 = int.Parse(anställd2);
+            Console.WriteLine("Vad är lönen för den trdje anställd? ");
+            string anställd3 = (Console.ReadLine());
+            int lön3 = int.Parse(anställd3);
+            Console.WriteLine("summan av de tre anställda är " + (lön1 + lön2 + lön3) + "kr");
+
+        }
+    }
+}
